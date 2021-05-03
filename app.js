e64e3de57dc66a571ad6f5f02f02e7769d45bfcc
DongJun-H <dongjun826@seoultech.ac.kr>
2021-05-03 20:22:09 +0900

    [ENH] 상품 정보 업로드에서 DB에 이미지 원본이름 저장 구현

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 //mongodb 사이트에서 카피한 주소
 const dbAddress =
-'mongodb+srv://EomYoosang:TlnXvxAlD3rRmdKz@0kcaldb.jkful.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+'mongodb+srv://dongjun826:eKKvisxRX9x6e7j8@zerocalorie.xguxz.mongodb.net/zeroCalorie?retryWrites=true&w=majority';
 
 mongoose
   .connect(dbAddress, {
