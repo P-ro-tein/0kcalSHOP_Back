232ed91ffa3f3d23d8a76b91ca94d74467647efa
DongJun-H <dongjun826@seoultech.ac.kr>
2021-05-04 21:11:38 +0900

    [CLN] 전반적인 라우터 수정 및 요청/응답 방식 변경

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+//mongodb 사이트에서 카피한 주소
+const dbAddress =
+    'mongodb+srv://dongjun826:eKKvisxRX9x6e7j8@zerocalorie.xguxz.mongodb.net/zeroCalorie?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI:'mongodb+srv://dongjun826:eKKvisxRX9x6e7j8@zerocalorie.xguxz.mongodb.net/zeroCalorie?retryWrites=true&w=majority'
+}
